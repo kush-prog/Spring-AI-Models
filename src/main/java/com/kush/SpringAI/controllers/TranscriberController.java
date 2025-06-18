156a84c22f668ff76ad2c2a908ca1f0006a146ee
kush-prog <chauhankush12345@gmail.com>
2025-06-18 10:19:14 +0530

    Audio trancriber implemented and project finished

--- src/main/java/com/kush/SpringAI/controllers/TranscriberController.java
+++ src/main/java/com/kush/SpringAI/controllers/TranscriberController.java
@@ -5,11 +5,10 @@ import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,12 +23,11 @@ public class TranscriberController {
 
     private final OpenAiAudioTranscriptionModel transcriptionModel;
 
-    public TranscriberController(@Value("${spring.ai.openai.api-key}") String apiKey) {
-        OpenAiAudioApi openAiAudioApi = new OpenAiAudioApi(apiKey);
-        this.transcriptionModel
-                = new OpenAiAudioTranscriptionModel(openAiAudioApi);
+    public TranscriberController(OpenAiAudioTranscriptionModel transcriptionModel) {
+        this.transcriptionModel = transcriptionModel;
     }
 
+    @PostMapping
     public ResponseEntity<String> transcribeAudio(@RequestParam("file") MultipartFile file) throws IOException{
         File tempFile = File.createTempFile("audio", ".wav");
         file.transferTo(tempFile);
